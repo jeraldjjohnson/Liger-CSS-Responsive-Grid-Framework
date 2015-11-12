f6e7feef645ccadb29cffc836de8004eb371d93f
jeraldjjohnson <Jerald@MacBooks-MBP.westell.com>
2015-11-11 21:49:54 -0800

    Liger Responsive Grid
    Beta version.

---
+++ js/liger.js
@@ -0,0 +1,70 @@
+$(function() {
+	animInput();
+	slider();
+
+});
+
+
+
+function animInput() {
+  
+  
+   $('.animated .input-group input').focusout(function(){
+    
+    var text_val = $(this).val();
+    
+    if(text_val === "") {
+      
+      $(this).removeClass('has-value');
+      
+    } else {
+      
+      $(this).addClass('has-value');
+      
+    }
+  });
+  
+}
+
+
+
+function slider() {
+  
+  
+var currentIndex = 0,
+  items = $('.slider div'),
+  itemAmt = items.length;
+
+function cycleItems() {
+  var item = $('.slider div').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
+
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 3000);
+
+$('.--next').click(function() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+$('.--prev').click(function() {
+  clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+  });
+}
+    
